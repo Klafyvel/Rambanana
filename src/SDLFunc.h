dd4f25b7ea318ad2405ee0818da523308e79c82d
Klafyvel <klafyvel@gmail.com>
2014-01-18 18:18:57 +0100

    Ajout de fonctions pour interagir avec SDL, toujours en provenance du même tutoriel

--- src/SDLFunc.h
+++ src/SDLFunc.h
@@ -2,5 +2,7 @@
 #define H_SDL_Func
 
 void logSDLError(std::ostream &os, const std::string &msg);
-
+SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *ren);
+void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y);
+void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, SDL_Rect *clip = nullptr);
 #endif
