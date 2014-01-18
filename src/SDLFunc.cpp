4017ab1b00836fecd221f9f5cc1fe6906de81ff9
Klafyvel <klafyvel@gmail.com>
2014-01-18 18:16:40 +0100

    Ajout de fichiers

---
+++ src/SDLFunc.cpp
@@ -0,0 +1,41 @@
+#include "SDL_Func.h"
+void logSDLError(std::ostream &os, const std::string &msg){
+	os << msg << " error: " << SDL_GetError() << std::endl;
+}
+
+SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *ren){
+	SDL_Texture *texture = nullptr;
+	SDL_Surface *loadedImage = SDL_LoadBMP(file.c_str());
+	if (loadedImage != nullptr){
+		texture = SDL_CreateTextureFromSurface(ren, loadedImage);
+		SDL_FreeSurface(loadedImage);
+		if (texture == nullptr)
+			logSDLError(std::cout, "CreateTextureFromSurface");
+	}
+	else
+		logSDLError(std::cout, "LoadBMP");
+
+	return texture;
+}
+
+void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y){
+	SDL_Rect dst;
+	dst.x = x;
+	dst.y = y;
+	SDL_QueryTexture(tex, NULL, NULL, &dst.w, &dst.h);
+	SDL_RenderCopy(ren, tex, NULL, &dst);
+}
+void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, SDL_Rect *clip = nullptr)
+{
+	SDL_Rect dst;
+	dst.x = x;
+	dst.y = y;
+	if (clip != nullptr){
+		dst.w = clip->w;
+		dst.h = clip->h;
+	}
+	else
+		SDL_QueryTexture(tex, NULL, NULL, &dst.w, &dst.h);
+
+	renderTexture(tex, ren, dst, clip);
+}
