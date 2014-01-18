5683f1c904188c4e3d44368c0deceb30cbcab99b
Klafyvel <klafyvel@gmail.com>
2014-01-18 18:15:57 +0100

    Définition de la classe personnage

--- src/personnage.h
+++ src/personnage.h
@@ -18,9 +18,35 @@
 #ifndef H_PERSONNAGE
 #define H_PERSONNAGE
 
+#include "defines.h"
+
 /* Contient la classe générique pour créer un personnage */
 class Personnage
 {
+public:
+	Personnage(int x, int y, int hitboxHeight, int hitboxWidth, char sprites[]);
+	void affiche(SDL_Renderer* render);
+	void avance(int direction); // voir description m_state 
+
+private:
+	int m_pointsDeVie;
+	
+	SDL_Texture m_sprites;
+	
+	int m_state; /* Varie entre 0 et 7:
+	
+	saute | cour | vaADroite || state
+		0		|		0	 |		0			 || 0
+		0		|		0	 |		1			 || 1
+		0		|		1	 |		0			 || 2
+		0		|		1	 |		1			 || 3
+		1		|		0	 |		0			 || 4
+		1		|		0	 |		1			 || 5
+		1		|		1	 |		0			 || 6
+		1		|		1	 |		1			 || 7
+*/		
+	
+	SDL_Rect m_hitbox;
 
 };
 
