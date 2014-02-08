a1f69d7a1225321f77940b6d63d41a2f83239330
Klafyvel <klafyvel@gmail.com>
2014-02-08 12:26:35 +0100

    Je ne sais pas écrire mon nom...

--- src/menu.h
+++ src/menu.h
@@ -1,4 +1,4 @@
-/*    Copyright (C) 2014 LEBY-FALK Hugo
+/*    Copyright (C) 2014 LEVY-FALK Hugo
 
     This program is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
